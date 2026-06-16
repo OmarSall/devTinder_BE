3ea14c0e7608ec0ffbbbcdf969789f4e2241301c
Omar Salloum <omarsalloum08@gmail.com>
2026-06-16 17:16:16 +0200

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get("/ab*c", (req,res) => {
+    res.send({firstName: "abc", lastName: "Saini"})
+});
+
 app.get("/user", (req,res) => {
     res.send({firstName: "Omar", lastName: "Saini"})
 });
