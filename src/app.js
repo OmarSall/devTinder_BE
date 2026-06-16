8f0b4df3a3e4648d7d484b73fb78ead7c19e20ce
Omar Salloum <omarsalloum08@gmail.com>
2026-06-16 22:51:04 +0200

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,34 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get("/ab*c", (req,res) => {
-    res.send({firstName: "abc", lastName: "Saini"})
-});
+app.use(
+    "/user",
+    (req, res, next) => {
+        console.log("Handling the route user!!")
+        next();
+        res.send("Response!!");  // server send the response and it will not go any further
 
-app.get("/user", (req,res) => {
-    res.send({firstName: "Omar", lastName: "Saini"})
-});
-
-app.post("/user", (req, res) => {
-    console.log("Save Data to the database");
-    res.send("Data successfully saved to the database!")
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully.")
-})
-
-app.use("/test",(req, res) => {
-    res.send("Test from the server");
-})
-
-app.use("/hello",(req, res) => {
-    res.send("Hello from the server");
-})
-
-app.use("/",(req, res) => {
-    res.send("Hello from the dashboard");
-})
+    },
+    (req, res) => {
+        console.log("Handling the route user 2!!!")
+        res.send("2nd Response!!")
+    }
+)
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
