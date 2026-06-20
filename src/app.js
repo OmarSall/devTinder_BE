5ec4c2e1f410ba2326b7372311f07eb47707bc4f
Omar Salloum <omarsalloum08@gmail.com>
2026-06-20 18:37:15 +0200

    model validation and data sanitization

--- src/app.js
+++ src/app.js
@@ -17,6 +17,62 @@ app.post("/signup", async (req,res) => {
     }
 })
 
+// get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.findOne({ emailId: userEmail });
+
+        if (users.length === 0) {
+            res.status(404).send("User Not Found")
+        } else {
+            res.send(users);
+        }
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// Feed API - GET /feed - get all the users from the db
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users)
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// Delete a user from the db
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) return res.status(404).send("User not found");
+        res.send("User deleted successfully.")
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate({ _id: userId } ,data, {
+            returnDocument: "after",
+            runValidators: true,
+        });
+        res.send("User updated successfully")
+    } catch (err) {
+        res.status(400).send("UPDATE FAILED: " + err.message);
+    }
+})
+
 connectDB
     .then(() => {
         console.log("Database connection established...")
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,51 @@
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 50,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://img.magnific.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3407.jpg?w=1000",
+    },
+    about: {
+        type: String,
+        default: "This is a default value about the user!",
+    },
+    skills: {
+        type: [String]
     }
+}, {
+    timestamps: true
 })
 
 const userModel = mongoose.model("User", userSchema);
